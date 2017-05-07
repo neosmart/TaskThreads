931e38e18f9ad0bbfc5e978ad036b2ac1cf40f9f
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-05-07 09:15:37 -0500

    Corrected namespace for ThreadStateException (regression fix)

--- TaskThread/Exceptions.cs
+++ TaskThread/Exceptions.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace TaskThread
+namespace System.Threading
 {
-    class Exceptions
+    public class ThreadStateException : Exception
     {
-        public class ThreadStateException : Exception
-        {
-            public ThreadStateException(string message = "", Exception innerException = null)
-                : base(message, innerException)
-            { }
-        }
+        public ThreadStateException(string message = "", Exception innerException = null)
+            : base(message, innerException)
+        { }
     }
 }

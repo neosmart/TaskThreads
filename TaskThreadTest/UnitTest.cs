a1a739ce444d0235d2590801d4e2c1ed4ff33a0d
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-05-09 05:39:50 -0500

    Initial unit testing

---
+++ TaskThreadTest/UnitTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace TaskThreadTest
+{
+    [TestClass]
+    public class BasicTests
+    {
+        [TestMethod]
+        public void InitializeTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void ThreadCreateTest()
+        {
+            var thread = new Thread(() =>
+            {
+                Debug.WriteLine("Thread started");
+            });
+            thread.Start();
+        }
+
+        [TestMethod]
+        public void ValidateThreadWork()
+        {
+            var workDone = new ManualResetEventSlim(false);
+
+            int x = 0;
+            var thread = new Thread(() =>
+            {
+                x = 42;
+                workDone.Set();
+            });
+
+            Assert.AreNotEqual(x, 42);
+            thread.Start();
+            workDone.Wait();
+            Assert.AreEqual(x, 42);
+        }
+
+        [TestMethod]
+        public void TestThreadJoin()
+        {
+            int x = 0;
+            var thread = new Thread(() =>
+            {
+                //await Task.Delay(5000);
+                x = 42;
+            });
+
+            Assert.AreNotEqual(x, 42);
+            thread.Start();
+            thread.Join();
+            Assert.AreEqual(x, 42);
+        }
+    }
+}
